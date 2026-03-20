9eafa4a7f0bbb36751c044fcf490dff841882189
VAIO73 <50487331+Vaso73@users.noreply.github.com>
2026-03-20 14:44:13 +0100

    Add MainWindow logic to the PingMonitor application

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+using System.Windows;
+
+namespace PingMonitor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnStartPing(object sender, RoutedEventArgs e)
+        {
+            // Logic to start ping monitoring
+        }
+
+        private void OnStopPing(object sender, RoutedEventArgs e)
+        {
+            // Logic to stop ping monitoring
+        }
+
+        private void OnShowResults(object sender, RoutedEventArgs e)
+        {
+            // Logic to display ping results
+        }
+    }
+}
