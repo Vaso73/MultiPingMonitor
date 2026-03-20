d320dfe168aa0d4f7e761791ffabac62b8d28736
VAIO73 <50487331+Vaso73@users.noreply.github.com>
2026-03-20 14:45:16 +0100

    Create PingMonitor/Classes/Constants.cs file with application constants

---
+++ PingMonitor/Classes/Constants.cs
@@ -0,0 +1,10 @@
+// Application Constants
+
+namespace PingMonitor.Classes
+{
+    public static class Constants
+    {
+        public const string CurrentDateTime = "2026-03-20 13:43:05";
+        // Add other constants here as needed
+    }
+}
